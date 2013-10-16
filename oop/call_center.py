30e080274f9e4e58fb0237ecea8348768638255d
Dmitry Mekhantev <mekhantev@live.com>
2013-10-17 00:43:54 +0400

    Add call center implementation

---
+++ oop/call_center.py
@@ -0,0 +1,96 @@
+from abc import ABCMeta
+
+__author__ = 'Dmitry Mekhantev'
+
+
+class Rank():
+    low = 0,
+    normal = 1,
+    high = 2
+
+
+class Singleton(type):
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instance
+
+
+class Call():
+    def __init__(self, rank: int):
+        self.rank = rank
+
+
+class Employee(metaclass=ABCMeta):
+    @property
+    def busy(self) -> bool:
+        return self._current_call is not None
+
+    @property
+    def current_call(self) -> Call:
+        return self._current_call
+
+    def __init__(self):
+        self._current_call = None
+        self.rank = None
+
+    def receive_call(self, call: Call):
+        if not self.busy:
+            self._current_call = call
+
+
+class Operator(Employee):
+    def __init__(self):
+        super().__init__()
+        self.rank = 0
+
+
+class Manager(Employee):
+    def __init__(self):
+        super().__init__()
+        self.rank = 1
+
+
+class Director(Employee):
+    def __init__(self):
+        super().__init__()
+        self.rank = 2
+
+
+class CallHandler(metaclass=Singleton):
+    @property
+    def operators(self) -> tuple:
+        return tuple(self._employee_levels[0])
+
+    @property
+    def managers(self) -> tuple:
+        return tuple(self._employee_levels[1])
+
+    @property
+    def directors(self) -> tuple:
+        return tuple(self._employee_levels[2])
+
+    def __init__(self):
+        self._employee_levels = [
+            [Operator() for _ in range(5)],
+            [Manager() for _ in range(2)],
+            [Director() for _ in range(1)]
+        ]
+
+    def dispatch_call(self, call: Call):
+        employee = self._find_not_busy_employee(call.rank)
+        employee.receive_call(call)
+
+    def _find_not_busy_employee(self, call_rank) -> Employee:
+        for rank in range(call_rank, 3):
+            for employee in self._employee_levels[rank]:
+                if not employee.busy:
+                    return employee
+        raise Exception('All employees are busy')
+
+
+
+
+
---
+++ tests/test_call_center.py
@@ -0,0 +1,21 @@
+from unittest.case import TestCase
+from oop.call_center import CallHandler, Call, Rank
+
+__author__ = 'Dmitry Mekhantev'
+
+
+class TestCallHandler(TestCase):
+    def test_dispatch_call(self):
+        handler = CallHandler()
+        call = Call(0)
+        handler.dispatch_call(call)
+        self.assertEqual(handler.operators[0].current_call, call)
+        call = Call(2)
+        handler.dispatch_call(call)
+        self.assertEqual(handler.directors[0].current_call, call)
+        call = Call(1)
+        handler.dispatch_call(call)
+        self.assertEqual(handler.managers[0].current_call, call)
+        for i in range(5):
+            handler.dispatch_call(Call(0))
+        self.assertRaises(Exception, handler.dispatch_call, Call(0))
