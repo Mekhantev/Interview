80c0444b5f22d1387754169da4ccb81956c5fa2f
Dmitry Mekhantev <mekhantev@live.com>
2013-10-18 22:41:07 +0400

    Change first function

--- utils.py
+++ utils.py
@@ -1,12 +1,7 @@
 __author__ = 'Dmitry Mekhantev'
 
 
-def _identity(item):
-    return item
-
-
-def first(iterable, predicate=_identity):
+def first(iterable):
     for item in iterable:
-        if predicate(item):
-            return item
+        return item
     raise ValueError('No satisfactory value found')
