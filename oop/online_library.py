10333d74ef61c39ca514706986780571b8687d71
Dmitry Mekhantev <mekhantev@live.com>
2013-10-19 02:10:48 +0400

    Add online library

---
+++ oop/online_library.py
@@ -0,0 +1,61 @@
+__author__ = 'Dmitry Mekhantev'
+
+
+class Book():
+    def __init__(self, author, title, body):
+        self.author = author
+        self.title = title
+        self.body = body
+
+
+class User():
+    @property
+    def login(self):
+        return self._login
+
+    def __init__(self, login):
+        self._login = login
+        self.current_book = None
+
+
+class OnlineLibrary():
+    def __init__(self):
+        self._books = {}
+        self._add_book('Ray Bradbury', 'Fahrenheit 451', 'aaa')
+        self._add_book('George Orwell', '1984', 'bbb')
+        self._add_book('Aldous Huxley', 'Brave New World','ccc')
+        self._users = {}
+        self._active_users = []
+
+    def register(self, login, password):
+        key = self._get_key(login, password)
+        user = self._users.get(key, None)
+        if user:
+            raise Exception('User with this login already exists')
+        self._users[key] = User(login)
+
+    def login(self, login_, password) -> User:
+        user = self._users.get(self._get_key(login_, password), None)
+        if user is None:
+            raise Exception('Wrong auth data')
+        self._active_users.append(user)
+        return user
+
+    def logout(self, user: User):
+        self._active_users.remove(user)
+
+    def get_book(self, user: User, author: str, title: str):
+        if not user in self._active_users:
+            raise Exception('Login first')
+        book = self._books.get(self._get_key(author, title), None)
+        if book is None:
+            raise Exception('Book not found')
+        user.current_book = book
+
+    def _get_key(self, primary: str, secondary: str):
+        return hash(primary + ' ' + secondary)
+
+    def _add_book(self, author, title, body):
+        book = Book(author, title, body)
+        key = self._get_key(author, title)
+        self._books[key] = book
---
+++ tests/test_online_library.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+from oop.online_library import *
+
+__author__ = 'Dmitry Mekhantev'
+
+
+class TestOnlineLibrary(TestCase):
+    def test_register(self):
+        ol = OnlineLibrary()
+        login = 'Dmitry'
+        password = 'abcdef'
+        ol.register(login, password)
+        user = ol.login(login, password)
+        self.assertEqual(login, user.login)
+        self.assertRaises(Exception, ol.register, login, password)
+
+    def test_login(self):
+        ol = OnlineLibrary()
+        login = 'Dmitry'
+        password = 'abcdef'
+        ol.register(login, password)
+        user = ol.login(login, password)
+        self.assertEqual(login, user.login)
+        self.assertRaises(Exception, ol.login, 'a', 'b')
+
+    def test_logout(self):
+        ol = OnlineLibrary()
+        login = 'Dmitry'
+        password = 'abcdef'
+        ol.register(login, password)
+        user = ol.login(login, password)
+        self.assertEqual(login, user.login)
+        ol.logout(user)
+        self.assertEqual(0, len(ol._active_users))
+
+    def test_get_book(self):
+        ol = OnlineLibrary()
+        login = 'Dmitry'
+        password = 'abcdef'
+        ol.register(login, password)
+        user = ol.login(login, password)
+        self.assertEqual(login, user.login)
+        author = 'George Orwell'
+        title = '1984'
+        ol.get_book(user, author, title)
+        self.assertEqual(user.current_book.author, author)
+        self.assertEqual(user.current_book.title, title)
+        ol.logout(user)
+        self.assertRaises(Exception, ol.get_book, user, author, title)
+        user = ol.login(login, password)
+        self.assertRaises(Exception, ol.get_book, user, 'a', title)
+
+
