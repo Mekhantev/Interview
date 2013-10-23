1ec2a37f2541d412d8684e1b27eae364f44e2327
Dmitry Mekhantev <mekhantev@live.com>
2013-10-24 02:26:27 +0400

    Add get path

--- recursion.py
+++ recursion.py
@@ -19,3 +19,30 @@ def count_ways_dynamic(n, l: list):
                 + count_ways_dynamic(n - 2, l)
                 + count_ways_dynamic(n - 3, l))
     return l[n]
+
+
+class Field():
+    def __init__(self, i=10):
+        self._matrix = []
+        for _ in range(i):
+            self._matrix.append([None for _ in range(i)])
+
+    def get_path(self, t: tuple, path: list):
+        path.append(t)
+        x = t[0]
+        y = t[1]
+        if x == 0 and y == 0:
+            return True
+        next_p = (x - 1, y)
+        b = False
+        if x > 0 and self._is_free(next_p):
+            b = self.get_path(next_p, path)
+        next_p = (x, y - 1)
+        if not b and y > 0 and self._is_free(next_p):
+            b = self.get_path(next_p, path)
+        #if not b:
+        #    path.remove(t)
+        return b
+
+    def _is_free(self, t: tuple):
+        return True
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -13,3 +13,17 @@ class TestRecursion(TestCase):
         i = 10
         ways = count_ways_dynamic(i, [None for _ in range(i + 1)])
         self.assertEqual(274, ways)
+
+
+class TestField(TestCase):
+    def test_get_path(self):
+        f = Field()
+        path = []
+        point = (7, 8)
+        expected_path = []
+        for i in range(0, point[0] + 1):
+            expected_path.append((point[0] - i, point[1]))
+        for i in range(1, point[1] + 1):
+            expected_path.append((0, point[1] - i))
+        self.assertTrue(f.get_path(point, path))
+        self.assertEqual(path, expected_path)
