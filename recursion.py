93bc3368de4b677d07ad8c0aba9b8e5593459959
Dmitry Mekhantev <mekhantev@live.com>
2013-10-23 19:21:28 +0400

    Add count ways dynamic

--- recursion.py
+++ recursion.py
@@ -7,3 +7,15 @@ def count_ways(n):
     elif n == 0:
         return 1
     return count_ways(n - 1) + count_ways(n - 2) + count_ways(n - 3)
+
+
+def count_ways_dynamic(n, l: list):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    elif not l[n]:
+        l[n] = (count_ways_dynamic(n - 1, l)
+                + count_ways_dynamic(n - 2, l)
+                + count_ways_dynamic(n - 3, l))
+    return l[n]
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from recursion import count_ways
+from recursion import *
 
 __author__ = 'Dmitry Mekhantev'
 
@@ -7,4 +7,9 @@ __author__ = 'Dmitry Mekhantev'
 class TestRecursion(TestCase):
     def test_count_ways(self):
         ways = count_ways(10)
+        self.assertEqual(274, ways)
+
+    def test_count_ways_dynamic(self):
+        i = 10
+        ways = count_ways_dynamic(i, [None for _ in range(i + 1)])
         self.assertEqual(274, ways)
