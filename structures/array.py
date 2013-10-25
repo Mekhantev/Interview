0b8b44f0653ac608d36bc973018e95b0ce55f551
Dmitry Mekhantev <mekhantev@live.com>
2013-10-25 18:15:31 +0400

    Modify get all subsets of set

--- structures/array.py
+++ structures/array.py
@@ -13,13 +13,14 @@ def binary_search(ints: 'Sorted linear collection',
 
 def get_subsets(ints: []) -> []:
     all_subsets = []
-    #the same as multiplying 1 by 2**len(ints)
+    #the same as 1 * 2**len(ints)
     subsets_number = 1 << len(ints)
     for bitmask in range(0, subsets_number):
+        #convert bitmask to set
         subset = []
-        for j in range(0, len(ints)):
-            #same as //'ing bitmask by 2**j
-            if ((bitmask >> j) & 1) == 1:
-                subset.append(ints[j])
+        for i in range(0, len(ints)):
+            #same as bitmask // 2**i
+            if ((bitmask >> i) & 1) == 1:
+                subset.append(ints[i])
         all_subsets.append(subset)
     return all_subsets
