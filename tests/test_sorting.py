00c0f07740a1cc735d91508b30f2f92c88a8f5cf
Dmitry Mekhantev <mekhantev@live.com>
2013-11-02 01:58:20 +0400

    Refactor merge lists test

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -6,6 +6,6 @@ class TestSorting(TestCase):
     def test_merge(self):
         la = [1, 4, 6, 10, 15, 20]
         lb = [-3, -1, 0, 2, 3, 7, 9, 11, 12, 22, 25]
+        expected = sorted(la + lb)
         merge(la, lb)
-        expected = [-3, -1, 0, 1, 2, 3, 4, 6, 7, 9, 10, 11, 12, 15, 20, 22, 25]
         self.assertEqual(la, expected)
