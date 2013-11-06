6fa223fa6bb4d5a7210a2fa9b958576f425411c0
Dmitry Mekhantev <mekhantev@live.com>
2013-11-07 02:05:07 +0400

    Refactor sorting

--- structures/array.py
+++ structures/array.py
@@ -21,3 +21,73 @@ def get_subsets(ints: []) -> []:
                 subset.append(ints[i])
         all_subsets.append(subset)
     return all_subsets
+
+
+def merge(la: list, lb: list):
+    lb_i = 0
+    la_i = 0
+    la_end = len(la) - 1
+    while la_i <= la_end:
+        while lb[lb_i] < la[la_i]:
+            la.insert(la_i, lb[lb_i])
+            lb_i += 1
+            la_i += 1
+            la_end += 1
+        la_i += 1
+    lb_end = len(lb)
+    while lb_i < lb_end:
+        la.append(lb[lb_i])
+        lb_i += 1
+
+
+def search(ints: list, left, right, x) -> int:
+    mid = (left + right) // 2
+    if x == ints[mid]:
+        return mid
+
+    if ints[left] < ints[mid]:
+        if ints[left] <= x <= ints[mid]:
+            return search(ints, left, mid - 1, x)
+        else:
+            return search(ints, mid + 1, right, x)
+    elif ints[left] > ints[mid]:
+        if ints[mid] <= x <= ints[right]:
+            return search(ints, mid + 1, right, x)
+        else:
+            return search(ints, left, mid - 1, x)
+
+
+def sort_strings_by_anagram(strings: list) -> list:
+    return sorted(strings, key=sorted)
+
+
+def search_string(strings: list, left, right, s) -> int:
+    mid = (left + right) // 2
+
+    if not strings[mid]:
+        l = mid - 1
+        r = mid + 1
+        while True:
+            if l < left and r > right:
+                raise IndexError()
+            elif l >= left and strings[l]:
+                mid = l
+                break
+            elif r <= right and strings[r]:
+                mid = r
+                break
+            l -= 1
+            r += 1
+
+    if s == strings[mid]:
+        return mid
+
+    if s > strings[mid]:
+        return search_string(strings, mid + 1, right, s)
+    else:
+        return search_string(strings, left, mid - 1, s)
+
+
+def sort_by_height_width(people):
+    sorted_people = sorted(people, key=lambda h: (h[0], h[1]))
+    return sorted_people
--- tests/test_array.py
+++ tests/test_array.py
@@ -18,3 +18,58 @@ class TestArray(TestCase):
             [1, 2, 3]
         ]
         self.assertEqual(expected_result, subsets)
+
+    def test_merge(self):
+        la = [1, 4, 6, 10, 15, 20]
+        lb = [-3, -1, 0, 2, 3, 7, 9, 11, 12, 22, 25]
+        expected = sorted(la + lb)
+        merge(la, lb)
+        self.assertEqual(la, expected)
+
+    def test_search(self):
+        ints = [11, 16, 20, 23, 27, 30, 1, 5, 7]
+        left = 0
+        right = len(ints) - 1
+        first_number = 27
+        second_number = 11
+        self.assertEqual(4, search(ints, left, right, first_number))
+        self.assertEqual(0, search(ints, left, right, second_number))
+        for _ in range(3):
+            ints.append(ints.pop(0))
+        self.assertEqual(1, search(ints, left, right, first_number))
+        self.assertEqual(6, search(ints, left, right, second_number))
+
+    def test_sort_strings_by_anagram(self):
+        strings = [
+            'abc',
+            'abcd',
+            'bcd',
+            'bac',
+            'badc',
+            'abcdef',
+            'adbc'
+        ]
+        expected = [
+            'abc',
+            'bac',
+            'abcd',
+            'badc',
+            'adbc',
+            'abcdef',
+            'bcd'
+        ]
+        result = sort_strings_by_anagram(strings)
+        self.assertEqual(expected, result)
+
+    def test_search_string(self):
+        strings = ['abc', '', 'bcd', '', '', 'ghj', 'nfm', '', 'pos', 'sdq']
+        s = 'pos'
+        self.assertEqual(strings.index(s), search_string(strings, 0, len(strings) - 1, s))
+        s = 'abc'
+        self.assertEqual(strings.index(s), search_string(strings, 0, len(strings) - 1, s))
+        s = 'cba'
+        self.assertRaises(IndexError, search_string, strings, 0, len(strings) - 1, s)
+
+    def test_sort_by_height_width(self):
+        people = [(200, 120), (170, 62), (180, 75), (190, 112), (175, 55), (186, 80), (170, 60), (190, 110)]
+        self.assertEqual(sorted(people, key=lambda h: (h[0], h[1])), sort_by_height_width(people))
